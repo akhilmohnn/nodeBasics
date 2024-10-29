e98bdc555ae0cd909a2769929f3aed8c16467231
Akhil Mohanan <akhilmohnn@outlook.com>
2024-10-29 22:28:50 +0530

    first pgm final

--- hello.js
+++ hello.js
@@ -1,8 +1,10 @@
-var name1="first program"
+var name1="First program"
 var num=10
 var name2=new String("String value") //cntrl+string can open definition
+//"new String" while assigning an string to object,it is same for all other with keyword new
 
 console.log(name1+" Hello world "+name1)
 console.log("Number is:"+num)
 
+console.log(name1.toLowerCase())
 console.log(name1.length)
