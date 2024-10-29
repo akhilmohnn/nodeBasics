e406ede194e3d807e2472afd48e727c780c5787f
Akhil Mohanan <akhilmohnn@outlook.com>
2024-10-29 22:22:07 +0530

    first pgm updated

--- hello.js
+++ hello.js
@@ -1,5 +1,8 @@
 var name1="first program"
 var num=10
+var name2=new String("String value") //cntrl+string can open definition
 
 console.log(name1+" Hello world "+name1)
 console.log("Number is:"+num)
+
+console.log(name1.length)
