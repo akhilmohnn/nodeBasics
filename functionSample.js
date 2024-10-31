a2167d51e874acf0aad9b3dcf704dd6492119759
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-01 00:56:25 +0530

    Basic function sample

---
+++ functionSample.js
@@ -0,0 +1,9 @@
+function hello(){
+    console.log("Hello")
+    return 10;
+}
+
+//can simply call by hello() or we can assingn it to another,and call that in console
+val=hello()
+
+console.log(val)
