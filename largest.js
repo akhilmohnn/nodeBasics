bef7163a5fe1b6a35d3ce0a5b53db636b43052c6
Akhil Mohanan <akhilmohnn@outlook.com>
2024-10-31 23:55:10 +0530

    largest.js updated

--- largest.js
+++ largest.js
@@ -6,6 +6,9 @@ var num2=read.question("Enter num2:")
 if(num1>num2){
     console.log("Largest is(num1): "+num1)
 }
+else if(num1===num2){ //3 equals for used for checking equal to
+    console.log("Both are same")
+}
 else{
     console.log("Largest is(num2): "+num2)
 }
