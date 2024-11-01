81588071ee6752516ef3e9187a67b03283c7364c
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-01 23:24:22 +0530

    Sum() working successfully

--- sumoftwoFunction.js
+++ sumoftwoFunction.js
@@ -1,9 +1,8 @@
 var read=require("readline-sync")
-void sum1();
-void sum2(n1,n2);
 
-var num1=parseInt(read.question("Enter num1:"))
-var num2=parseInt(read.question("Enter num2"))
+
+// var num1=parseInt(read.question("Enter num1:"))
+// var num2=parseInt(read.question("Enter num2"))
 
 while(1){
     console.log("1.Sum using sum1()\n2.sum using sum2(num1,num2)\n3.Sum using sum3()\n4.Sum using sum4(num1,num2)")
@@ -19,6 +18,17 @@ while(1){
     }
     
     return 0;
+}
+
+function sum1()
+{
+    var a,b;
+    var a=parseInt(read.question("Enter num1"))
+    var b=parseInt(read.question("Enter num2"))
+    sum=a+b
+    console.log("Sum from sum1 is:"+sum)
+
+
 }
 
 
