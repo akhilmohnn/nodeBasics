075ad6b477cb423423fb214e0e209491994eeabe
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-02 12:58:46 +0530

    Array operation

---
+++ array.js
@@ -0,0 +1,14 @@
+var read=require("readline-sync")
+
+var arr=["1","2","3","4","5"]
+
+console.log(arr)
+
+var a=[];
+console.log("Enter 5 numbers:")
+for(let i=0;i<5;i++){
+    a[i] = parseInt(read.question(`number${(i+1)}: `,10))
+
+}
+
+console.log(a)
