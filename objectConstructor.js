e782284d6bf3b59a38450e26be97153af03716fa
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-03 12:36:23 +0530

    simple Object Constructor created

---
+++ objectConstructor.js
@@ -0,0 +1,20 @@
+/*object constructor function allows you to create multiple instances 
+          of an object type with the same properties and methods*/
+
+function Student(name,age,place){
+    this.name=name //placing values in object's name,age and place using this
+    this.age=age
+    this.place=place
+    this.display= function(){
+        console.log("Name:"+this.name+"\nAge:"+this.age+"\nplace:"+this.place)
+    }
+    
+}
+
+// Creating a new instance of Student
+var student1=new Student("Akhil",20,"Kochi")
+
+// Calling the display method on student1,which is a function
+student1.display()
+
+
