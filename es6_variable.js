4ab89e7890bb265f3733f281c4dad2eba38f9e42
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-04 00:30:02 +0530

    es6 let and var difference stated

--- es6_variable.js
+++ es6_variable.js
@@ -17,11 +17,13 @@ function topFunction(){
     if(i==0){
         var j=1
         let hey=200
+        var i=400
         console.log("Value of hey: "+hey)  //it will only print inside this block
     }
 
     console.log(j) //it will print the j value that is a "var"
     //console.log(hey)   this will cause an error, hey is not accessible from here,it is only available inside the declared block
+    console.log("i modified:" +i)  //var values will be modified takes,the latest value if same name used
 }
 
 topFunction()
