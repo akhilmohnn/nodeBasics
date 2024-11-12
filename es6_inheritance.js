01e4002cd1952e0df487b761a980cd32bfd0d5ee
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-13 00:41:18 +0530

    Inheritance is used here in simple format

---
+++ es6_inheritance.js
@@ -0,0 +1,20 @@
+class outerClass {
+    sampleHello() {
+        console.log("This is a sample program");
+    }
+}
+
+class innerClass extends outerClass {
+    constructor(num1, num2) {
+        super();
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    hello() {
+        console.log("hello " + (this.num1 + this.num2));
+    }
+}
+
+let hey = new innerClass(10, 20);
+hey.hello();          // Calls hello() from innerClass
+hey.sampleHello();    // Calls sampleHello() from outerClass
