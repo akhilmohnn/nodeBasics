67584965469dfc7228ed5e3e1340ff531d9d9380
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-14 00:10:48 +0530

    arrowFunction usage

---
+++ es6_arrowFunction.js
@@ -0,0 +1,8 @@
+function add(a,b){
+    return a+b
+}
+
+console.log(add(10,20)) //normal fn. declaration and calling
+
+let add1=(a,b) => a+b  //if single no brace,return required otherwise wants
+console.log(add1(30,20)) //declaring and calling arrow function
