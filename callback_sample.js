c2b87af20ee8eea4f14b8c12d137bcf22e6ec9da
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-17 21:03:40 +0530

    Callback: execution time for a loop is passed and evaluated

--- callback_sample.js
+++ callback_sample.js
@@ -9,3 +9,14 @@ for(i=0;i<100;i++){
 console.log("End")
 var diff=new Date()-d
 console.log(diff)
+
+function longTask(milliSeconds){
+    var dt=new Date()
+    while(new Date-dt <= milliSeconds){
+
+    }
+}
+
+console.log("Started")
+longTask(4000) //this will take 4 seconds,and then prints the next line
+console.log("Ended")
