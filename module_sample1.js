d91bc4657ea4099b3bf6d95c154d6e6f4c082bae
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-19 00:02:53 +0530

    Added one more example for local module calling in another

--- module_sample1.js
+++ module_sample1.js
@@ -1,3 +1,5 @@
 module.exports.hello=function(){   //hello named function is used for export
     console.log("Hello friends")
 }
+
+module.exports.hey=10
--- module_sample2.js
+++ module_sample2.js
@@ -1,3 +1,5 @@
 var h=require("./module_sample1")
 
 h.hello()
+
+console.log(h.hey)
