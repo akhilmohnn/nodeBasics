1dbe54098a09a3d3911aa65bece7954ba8ba8e9e
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-20 21:59:05 +0530

    Reading a html file: file system module fs

--- FileSystemModule/sample.js
+++ FileSystemModule/sample.js
@@ -3,7 +3,7 @@ var fs=require('fs')  //fs for taking another, file system module
 
 http.createServer(function (req,res){
 
-    fs.readFile('sample.html',function(err,data){
+    fs.readFile('sample.html',function(err,data){  //error coming possibility not considered
         res.writeHead(200,{'content-type':'text/html'})
         res.write(data)
         res.end()
